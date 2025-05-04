af4ca762037a0f8f483c9fac1c6ebad7e9eca110
jhonwii004 <jhonwii004@gmail.com>
2025-05-03 22:26:09 -0500

    changed stuff

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 export default {
-    plugins: {
-      "@tailwindcss/postcss": {},
-    }
+  plugins: {
+    '@tailwindcss/postcss': {},  // Replace 'tailwindcss' with this
+    autoprefixer: {},
   }
+}
